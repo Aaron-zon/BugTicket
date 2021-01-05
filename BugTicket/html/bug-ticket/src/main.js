d9b108361887e1a0c0d9cb5599ce6461a275d561
Aaron-zon <1640485761@qq.com>
2021-01-05 18:02:45 +0800

    Aaron-zon
    添加jquery，bootstrap；

--- BugTicket/html/bug-ticket/src/main.js
+++ BugTicket/html/bug-ticket/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+window.$ = window.jQuery = $;
 
 Vue.config.productionTip = false
 
